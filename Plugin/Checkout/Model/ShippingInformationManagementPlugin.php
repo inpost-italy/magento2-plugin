4d2e92e26764a5db47a3bac8c26ed71ee9362d57
Andriy <andriy.kravets@forbytes.com>
2023-01-12 23:44:27 +0100

    Added address extension attribute.

---
+++ Plugin/Checkout/Model/ShippingInformationManagementPlugin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace InPost\Shipment\Plugin\Checkout\Model;
+
+use Magento\Quote\Model\QuoteRepository;
+use Magento\Checkout\Model\ShippingInformationManagement;
+use Magento\Checkout\Api\Data\ShippingInformationInterface;
+
+class ShippingInformationManagementPlugin
+{
+    /**
+     * Set Inpost Attribute Values
+     *
+     * @param  ShippingInformationManagement $subject
+     * @param                                $cartId
+     * @param  ShippingInformationInterface  $addressInformation
+     */
+    public function beforeSaveAddressInformation(
+        ShippingInformationManagement $subject,
+                                      $cartId,
+        ShippingInformationInterface $addressInformation
+    ) {
+        $shippingAddress = $addressInformation->getShippingAddress();
+        if ($shippingAddressExtensionAttributes = $shippingAddress->getExtensionAttributes()) {
+            $pointId = $shippingAddressExtensionAttributes->getInpostPointId();
+            $shippingAddress->setInpostPointId($pointId);
+        }
+    }
+}
